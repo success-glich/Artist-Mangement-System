e3dd0d331b661c9b1e61a8c40a92bd2fd34aba80
success21 <iamsaphal21@gmail.com>
2024-05-31 23:38:48 +0545

    initialize table if not exists

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,18 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
 import { checkConnection } from "./src/config/db";
+import createTables from "./src/config/setup";
 
 const port = config.port || 8001
 
 
 
 const startServer = async () => {
+
     //*  Check the database connection
       await checkConnection(); 
+    //* Initialize database 
+    await createTables()
   
     app.listen(port, () => {
       console.log(`Server is running on port :: ${port}`);
---
+++ server/src/config/setup.ts
@@ -0,0 +1,63 @@
+import pool from "./db";
+
+
+const createTables = async () => {
+    try {
+
+        await pool.query(`CREATE TABLE IF NOT EXISTS "admin"(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    username VARCHAR(255) UNIQUE,
+    password VARCHAR(500),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`)
+        await pool.query(`
+      CREATE TABLE IF NOT EXISTS "user" (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(255),
+        last_name VARCHAR(255),
+        email VARCHAR(255) UNIQUE,
+        password VARCHAR(500),
+        phone VARCHAR(20),
+        dob DATE,
+        gender CHAR(1) CHECK (gender IN ('m', 'f', 'o')),
+        address VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+        await pool.query(`
+      CREATE TABLE IF NOT EXISTS artist (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        dob DATE,
+        gender CHAR(1) CHECK (gender IN ('m', 'f', 'o')),
+        address VARCHAR(255),
+        first_release_year INT,
+        no_of_albums_released INT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+        await pool.query(`
+      CREATE TABLE IF NOT EXISTS music (
+        id SERIAL PRIMARY KEY,
+        artist_id INT REFERENCES artist(id),
+        title VARCHAR(255),
+        album_name VARCHAR(255),
+        genre VARCHAR(10) CHECK (genre IN ('rnb', 'country', 'classic', 'rock', 'jazz')),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+        console.log('Tables created successfully');
+    } catch (error) {
+        console.error('Error creating tables:', error);
+    }
+};
+
+export default createTables;
