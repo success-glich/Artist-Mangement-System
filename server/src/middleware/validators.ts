61576af563045579e8a6047530b24eba11f8e572
success21 <iamsaphal21@gmail.com>
2024-06-01 15:44:37 +0545

    add validator schema fro artist

--- server/src/middleware/validators.ts
+++ server/src/middleware/validators.ts
@@ -2,6 +2,13 @@ import { body, validationResult } from "express-validator";
 import { Request, Response, NextFunction } from "express";
 import { ApiResponse } from "../helper/ApiResponse";
 
+function validationCheck(req: Request, res: Response, next: NextFunction) {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json(new ApiResponse(400, { error: error.array() }));
+  }
+  next();
+}
 export const validateAdminRegister = [
   body("name").notEmpty().withMessage("Name is required"),
   body("username")
@@ -12,15 +19,7 @@ export const validateAdminRegister = [
   body("password")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters long"),
-  (req: Request, res: Response, next: NextFunction) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res
-        .status(400)
-        .json(new ApiResponse(400, { error: error.array() }));
-    }
-    next();
-  },
+  validationCheck,
 ];
 
 export const validateLogin = [
@@ -30,15 +29,7 @@ export const validateLogin = [
     .withMessage("Password is required")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters long"),
-  (req: Request, res: Response, next: NextFunction) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res
-        .status(400)
-        .json(new ApiResponse(400, { error: error.array() }));
-    }
-    next();
-  },
+  validationCheck,
 ];
 
 export const validateUser = [
@@ -86,20 +77,11 @@ export const validateUser = [
     .withMessage("Invalid gender"),
 
   body("address").notEmpty().withMessage("Address is required"),
-  (req: Request, res: Response, next: NextFunction) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res
-        .status(400)
-        .json(new ApiResponse(400, { error: error.array() }));
-    }
-    next();
-  },
+  validationCheck,
 ];
 
-export const validateUserUpdate =[
-
-    body("first_name")
+export const validateUserUpdate = [
+  body("first_name")
     .notEmpty()
     .withMessage("First name is required")
     .isLength({ max: 255 })
@@ -147,4 +129,43 @@ export const validateUserUpdate =[
     }
     next();
   },
-]
+];
+
+export const validateArtist = [
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ max: 255 })
+    .withMessage("Name must be less than 255 characters"),
+
+  body("dob")
+    .notEmpty()
+    .withMessage("Date of birth is required")
+    .isISO8601()
+    .withMessage("Date of birth must be a valid date"),
+
+  body("gender")
+    .notEmpty()
+    .withMessage("Gender is required")
+    .isIn(["m", "f", "o"])
+    .withMessage("Gender must be one of: m, f, o"),
+
+  body("address")
+    .notEmpty()
+    .withMessage("Address is required")
+    .isLength({ max: 255 })
+    .withMessage("Address must be less than 255 characters"),
+
+  body("first_release_year")
+    .notEmpty()
+    .withMessage("First release year is required")
+    .isInt({ min: 1900, max: new Date().getFullYear() })
+    .withMessage("First release year must be a valid year"),
+
+  body("no_of_albums_released")
+    .notEmpty()
+    .withMessage("Number of albums released is required")
+    .isInt({ min: 0 })
+    .withMessage("Number of albums released must be a non-negative integer"),
+  validationCheck,
+];
