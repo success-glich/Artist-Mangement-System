146fbd05bd841898a327703599edc8e86e343934
success21 <iamsaphal21@gmail.com>
2024-06-02 19:49:06 +0545

    add  zustand for token management

---
+++ client/store.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
+
+
+export interface TokenStore {
+    token: string;
+    setToken: (data: string) => void;
+}
+
+const useTokenStore = create<TokenStore>()(
+    devtools(
+      persist(
+        (set) => ({
+          token:"",
+          setToken: (data:string) => set(() => ({ token:data})),
+        }),
+        {
+          name: 'bear-storage',
+        },
+      ),
+    ),
+  );
+
+  export default useTokenStore;
