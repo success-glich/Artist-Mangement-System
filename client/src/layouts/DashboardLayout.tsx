463ca9706b44ea6dcadb747955492c986a840b43
success21 <iamsaphal21@gmail.com>
2024-06-03 09:30:06 +0545

    redirect logic for missing token in useTokenStore

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Navigate, Outlet } from "react-router-dom"
 import {
     Bell,
     CircleUser,
@@ -19,8 +19,13 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import useTokenStore from "@/store"
 
 function DashboardLayout() {
+    const { token } = useTokenStore();
+    if (!token) {
+        return <Navigate to="/auth/login" />
+    }
     return (
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
             <div className="hidden border-r bg-muted/40 md:block">
