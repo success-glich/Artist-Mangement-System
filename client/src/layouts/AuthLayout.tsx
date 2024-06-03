55be399b848d107c8d6d18a05aab026f8ef22b64
success21 <iamsaphal21@gmail.com>
2024-06-03 09:26:08 +0545

    add token-based navigation to dashboard home

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,7 +1,12 @@
 
-import { Outlet } from 'react-router-dom'
+import useTokenStore from '@/store';
+import { Navigate, Outlet } from 'react-router-dom'
 
 export const AuthLayout = () => {
+    const token = useTokenStore(state => state.token);
+    if (token) {
+        return <Navigate to="/dashboard/home" />
+    }
     return (
         <><Outlet /></>
     )
